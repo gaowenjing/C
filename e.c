3a393ccd83cbce4b89f3ae1adaf97efd4119f28b
Gaowenjing <gwj1984@gmail.com>
2011-04-01 22:14:18 +0800

    modified:   .vimrc 	deleted:    a 	deleted:    a.c 	deleted:    b 	deleted:    b.c 	deleted:    c 	deleted:    d 	modified:   e.c 	deleted:    helloworld 	deleted:    helloworld.c 	deleted:    structure 	deleted:    test 	deleted:    test.c 	deleted:    typedef 	deleted:    union

--- e.c
+++ e.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-//#include </home/gwj/lab/gwj.h>
-#include "xgwj.h"
+#define ZERO 0
 
 int main (int argc , char** argv){
 	printf ("%d\n",ZERO);
@@ -8,10 +7,10 @@ int main (int argc , char** argv){
 	printf ("%s\n",s);
 	printf ("%p\n",*s);
 	printf ("%p\n",&s);
-//	char t[0];
-//	scanf ("%s",t);
-//	perror ("scanf");
-//	printf ("%s\n",t);
+	char t[0];
+	scanf ("%s",t);
+	perror ("scanf");
+	printf ("%s\n",t);
 	if ( gwj() != 0 ) 
 		printf ("%u",sizeof(gwj()));
 	return 0;
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main (char**gwj) {
-	int i ;
-	for (i=0 ;  ;i++ ){
-		printf ("%s\n" , gwj[i]);
-	}
-//	printf ("%i\n" , gw );
-}
